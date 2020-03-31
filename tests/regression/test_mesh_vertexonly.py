1a7fde97fd06dd4436ae0a98525b88cedbdd6ada
Reuben Hill <ReubenHill@users.noreply.github.com>
2020-03-31 10:21:19 +0100

    add some kind of basic tests

--- tests/regression/test_mesh_vertexonly.py
+++ tests/regression/test_mesh_vertexonly.py
@@ -2,3 +2,28 @@ import pytest
 import numpy as np
 
 from firedrake import *
+
+def test_pic_swarm_in_plex():
+    m = UnitSquareMesh(1,1)
+    points = [(.1, .1), (.9, .9)]
+    plex = m.topology._plex
+    swarm = mesh._pic_swarm_in_plex(plex, points)
+    # Now somehow check that points are in each cell as expected
+    #TODO
+    # Then check that distribution has worked across MPI ranks as expected
+    #TODO
+
+@pytest.mark.parallel(nprocs=2)
+def test_pic_swarm_in_plex_parallel():
+    test_pic_swarm_in_plex()
+
+def test_generate():
+    m = UnitSquareMesh(5,5)
+    vertexcoords = [(.1, .1), (.2, .3), (.7, .8)]
+    vm = VertexOnlyMesh(m, vertexcoords)
+    vm.init()
+
+# remove this before final merge
+if __name__ == "__main__":
+    import pytest, sys
+    pytest.main([sys.argv[0]])
