a62928efad4b8a3e728082e5baa43a292b79426c
Reuben Hill <ReubenHill@users.noreply.github.com>
2020-04-15 12:30:51 +0100

    Revert "move to post-init callback"
    This reverts commit 5c87b1511ef7831e9bf0b3c098f0767bc726f213.

--- firedrake/mesh.py
+++ firedrake/mesh.py
@@ -1055,9 +1055,6 @@ class VertexOnlyMeshTopology(MeshTopology):
                 entity_dofs[0] = 1
                 self._vertex_numbering = self.create_section(entity_dofs)
 
-                if swarm.comm.size > 1:
-                    _pic_swarm_remove_ghost_cell_coords(self, self._swarm)
-
         self._callback = callback
 
     ## BEGIN TODO
@@ -1819,6 +1816,8 @@ def VertexOnlyMesh(mesh, vertexcoords, comm=COMM_WORLD):
         raise NotImplementedError("Only straight edged meshes are supported")
 
     swarm = _pic_swarm_in_plex(mesh.topology._plex, vertexcoords)
+    if swarm.comm.size > 1:
+        _pic_swarm_remove_ghost_cell_coords(mesh, swarm)
 
     # Topology
     topology = VertexOnlyMeshTopology(swarm, mesh.topology, name="swarmmesh", reorder=False)
@@ -1940,9 +1939,7 @@ def _pic_swarm_remove_ghost_cell_coords(parentmesh, dmswarm):
     immersed in `parentmesh` which are located in the ghost cells of
     `parentmesh`.
     """
-    point_sf = dmswarm.getPointSF()
-    nroots, local, remote = point_sf.getGraph()
-    print(f"Rank {dmswarm.comm.rank} has nroots {nroots}, local {local}, remote {remote}")
+    raise NotImplementedError("Removal of DMSwarm Coordinates from Ghost cells still WIP")
 
 def SubDomainData(geometric_expr):
     """Creates a subdomain data object from a boolean-valued UFL expression.
